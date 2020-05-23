1ea1981902b8b6452626c46c26ccc8efa0c8e364
Christian Studer <cstuder@existenz.ch>
2020-05-23 07:02:08 +0200

    Initial commit: Everything is done

---
+++ README.md
@@ -0,0 +1,27 @@
+# Aare.guru für BitBar
+
+Die aktuelle Aare-Temperatur in deiner MacOS-Menuzeile.
+
+## Installation
+
+- [BitBar](https://getbitbar.com) herunterladen und installieren.
+- [Installations-Link klicken](bitbar://openPlugin?title=Aare.guru&src=https://github.com/aareguru/aare.guru.bitbar/raw/master/aareguru.5m.php) oder manuell die Datei `aareguru.5m.php` in dein BitBar-Plugins-Verzeichnis kopieren.
+- Optional in der Datei deine Schmerzgrenze und einen anderen Ort konfigurieren, unterhalb der Zeile `// Configuration`.
+
+## Datenquelle
+
+Die Messdaten der Aare stammen vom [Bundesamt für Umwelt BAFU](https://www.hydrodaten.admin.ch).
+
+Die [API](https://aareguru.existenz.ch) ist für die Webseite [Aare.guru](https://aare.guru) geschrieben.
+
+## Verwandte Projekte
+
+- Offizielle [Firefox-Extension](https://addons.mozilla.org/de/firefox/addon/aare-guru/)
+- Offizielle [Chrome-Extension](https://chrome.google.com/webstore/detail/aareguru/oaicpaghidmlakfogpmjpopkjhncjjfe?hl=de)
+- [CLI-Client](https://github.com/gexclaude/aaregurucli/)
+- [MagiMagicMirror² Modul](https://github.com/buge/MMM-AareGuru)
+- [LaMetric App](https://apps.lametric.com/apps/aare_temperatur_anzeige__daten_von_aare_guru_/8544)
+
+## Lizenz
+
+MIT
---
+++ aareguru.5m.php
@@ -0,0 +1,68 @@
+#!/usr/bin/env php
+
+<?php
+# <bitbar.title>Aare.guru</bitbar.title>
+# <bitbar.version>v1.0</bitbar.version>
+# <bitbar.author>Christian Studer</bitbar.author>
+# <bitbar.author.github>cstuder</bitbar.author.github>
+# <bitbar.desc>Displays current Aare river temperature</bitbar.desc>
+# <bitbar.image>https://aare.guru/ios/gurulogo.svg</bitbar.image>
+# <bitbar.dependencies>php</bitbar.dependencies>
+# <bitbar.abouturl>https://aare.guru/</bitbar.abouturl>
+
+// Configuration
+$schmerzgrenze = 18; // [°C], switches color of the display when reached
+$city = "bern"; // For a list of all possible cities look here: https://aareguru.existenz.ch/v2018/cities
+
+// Internal configuration
+$version = '1.0.0';
+$app = 'aare.guru.bitbar';
+$coldcolor = '#0000a0';
+$warmcolor = '#009fb0';
+$coldcolor_darkmode = '#d1d1e0';
+$warmcolor_darkmode = '#00cc00';
+$apiurl = "https://aareguru.existenz.ch/v2018/today?city={$city}&app={$app}&version={$version}";
+
+// Fetch data the oldfashioned way
+$raw = @file_get_contents($apiurl);
+$data = @json_decode($raw);
+
+if (!$data) {
+    echo "Fehler vom Aare.guru\n";
+    echo "---\n";
+    echo "Tschuldigung.\n";
+    echo "Keine Antwort von der API. :-(";
+    exit(1);
+}
+
+// Display data
+$temperature = $data->aare;
+$text = $data->text;
+$longname = $data->longname;
+
+$ago = time() - $data->time;
+$agostring = '';
+
+if ($ago < 120) {
+    $agostring = "vor {$ago} Sekunden";
+} else if ($ago < (60 * 60)) {
+    $minutes = floor($ago / 60);
+    $agostring = "vor {$minutes} Minuten";
+} else {
+    $agostring = "vor längerer Zeit";
+}
+
+// Determine color
+$darkmode = getenv('BitBarDarkMode');
+
+if ($temperature >= $schmerzgrenze) {
+    $color = $darkmode ? $warmcolor_darkmode : $warmcolor;
+} else {
+    $color = $darkmode ? $coldcolor_darkmode : $coldcolor;
+}
+
+// Output
+echo "{$temperature}°|color={$color}\n";
+echo "---\n";
+echo "Gemessen {$agostring} in {$longname}\n";
+echo "{$text}|href=https://aare.guru/#{$city}";
